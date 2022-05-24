9a28dd88d468a4d9e68784298a7214362951cd01
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2022-05-24 14:25:36 +0530

    Create CycleDetectionUsingBFS.cpp

---
+++ CycleDetectionUsingBFS.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Sol
+{
+ public:
+ bool CheckForCycle(int s,int v,vector<int> adj[],vector<int>& visited)
+ {
+  queue<pair<int,int>> q;
+  
+  visited[s]=true;
+  q.push({s,-1});
+  
+  while(!q.empty()){
+  int node=q.front().first;
+  int par=q.front().second;
+  q.pop();
+  
+  for(auto it:adj[node]){
+    if(!visited[it]){
+    visited[it]=true;
+    q.push({it,node});
+    }
+    else if(par!=it)
+    return true;
+    
+  }
+  
+  }
+  return false;
+ }
+   
+   bool isCycle(int v,vector<int> adj[])
+   {
+   vector<int> visited(v+1,0);
+   for(int i=1;i<=v;i++){
+   if(!visited[i]){
+    if(CheckForCycle(i,v,adj,visited)) return true;
+   }
+   }
+   return false;
+   } 
+    
+};
+
+int main()
+{
+Sol s;
+int v,m;//number of vertices
+cout<<"Enter number of vertices : ";
+cin>>v;
+cout<<"\nEnter number of Edges ";
+cin>>m;
+vector<int> adj[v+1];//adjacency list
+
+for(int i=1;i<=m;i++)
+{
+int u,v;
+cin>>u>>v;
+adj[u].push_back(v);
+adj[v].push_back(u);
+}
+bool b=s.isCycle(v,adj);
+if(b==true)
+cout<<"\ncycle detected ";
+else
+cout<<"\ncycle not present";
+
+}    
